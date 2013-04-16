4ac13160ede145c726dca04e05afb34197068f57
Scott Fasone <scott181182@yahoo.com>
2013-04-15 21:09:44 -0300

    JavaDoc

--- Beginner/Episode-1/Main.java
+++ Beginner/Episode-1/Main.java
@@ -1,5 +1,13 @@
 package MMC;
 
+/**
+ * Main class for Episode 1 - Setup and Hello World
+ * 
+ * 	http://youtu.be/3bAa0fAFCpg
+ * 
+ * @author Scott Fasone
+ **/
+ 
 public class Main
 {
 	public static void main(String[] args)
